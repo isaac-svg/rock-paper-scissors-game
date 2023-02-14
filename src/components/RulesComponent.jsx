caff33d6e53e0fed38c94796fb48bc6d15f57762
isaac-svg <isaacsakyi009@gmail.com>
2023-02-14 21:56:42 +0000

    changed the directory of RulesComponent and creadted a style file for it

--- src/components/RulesComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RulesComponent = () => {
-  return <div>RulesComponent</div>;
-};
-
-export default RulesComponent;
---
+++ src/components/utils/RulesComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const RulesComponent = () => {
+  return (
+    <aside className="rules__container">
+      <div className="rules__card">
+        <img src="images/image-rules-bonus.svg" alt="rules image" />
+        <img src="images/icon-close.svg" alt="close button for rules card" />
+      </div>
+    </aside>
+  );
+};
+
+export default RulesComponent;
